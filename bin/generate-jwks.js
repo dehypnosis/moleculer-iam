476facd11be071dc43ebdb7e6c842106304b1da3
Dong Wook Kim <dehypnosis@gmail.com>
2019-12-04 15:07:15 +0900

    update generate-jwks to support node@^10.x.x

--- bin/generate-jwks.js
+++ bin/generate-jwks.js
@@ -5,22 +5,32 @@ const path = require("path");
 const jose = require("jose");
 
 const keystore = new jose.JWKS.KeyStore();
+const handleGenerationError = error => console.warn(`Warning:`, error.message);
 
 Promise.all([
-  keystore.generate("RSA", 2048, { use: "sig" }),
-  keystore.generate("RSA", 2048, { use: "enc" }),
-  keystore.generate("EC", "P-256", { use: "sig" }),
-  keystore.generate("EC", "P-256", { use: "enc" }),
-  keystore.generate("OKP", "Ed25519", { use: "sig" }),
-]).then(() => {
-  const filePath = path.resolve("./jwks.json");
-  try {
-    fs.statSync(filePath);
-  } catch {
-    fs.writeFileSync(filePath, JSON.stringify(keystore.toJWKS(true), null, 2));
-    console.log(`Succeed: ${filePath} created`);
-    process.exit(0);
-  }
-  console.error(`Error: ${filePath} already exists`);
-  process.exit(1);
-});
+  keystore.generate("RSA", 2048, {use: "sig"}).catch(handleGenerationError),
+  keystore.generate("RSA", 2048, {use: "enc"}).catch(handleGenerationError),
+  keystore.generate("EC", "P-256", {use: "sig"}).catch(handleGenerationError),
+  keystore.generate("EC", "P-256", {use: "enc"}).catch(handleGenerationError),
+  keystore.generate("OKP", "Ed25519", {use: "sig"}).catch(handleGenerationError),
+])
+  .then(() => {
+    if (keystore.all({ use: "sig" }).length === 0) {
+      console.error(`Error: empty signature keys`);
+      process.exit(1);
+    } else if (keystore.all({ use: "enc" }).length === 0) {
+      console.error(`Error: empty encryption keys`);
+      process.exit(1);
+    }
+
+    const filePath = path.resolve("./jwks.json");
+    try {
+      fs.statSync(filePath);
+    } catch (error) {
+      fs.writeFileSync(filePath, JSON.stringify(keystore.toJWKS(true), null, 2));
+      console.log(`Succeed: ${filePath} created`);
+      process.exit(0);
+    }
+    console.error(`Error: ${filePath} already exists`);
+    process.exit(1);
+  });
