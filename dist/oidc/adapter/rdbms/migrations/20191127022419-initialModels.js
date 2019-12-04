cc46593c7449b1e72c22cbf74029a2b5f49dafcb
Dong Wook Kim <dehypnosis@gmail.com>
2019-12-04 11:53:16 +0900

    allow js for dist

---
+++ dist/oidc/adapter/rdbms/migrations/20191127022419-initialModels.js
@@ -0,0 +1,36 @@
+'use strict';
+const tslib_1 = require("tslib");
+const { STRING, JSON, DATE } = require("sequelize");
+const OIDCModelNames = [
+    "Session",
+    "AccessToken",
+    "AuthorizationCode",
+    "RefreshToken",
+    "DeviceCode",
+    "ClientCredentials",
+    "Client",
+    "InitialAccessToken",
+    "RegistrationAccessToken",
+    "Interaction",
+    "ReplayDetection",
+    "PushedAuthorizationRequest",
+];
+const OIDCGrantModelNames = [
+    "AccessToken",
+    "AuthorizationCode",
+    "RefreshToken",
+    "DeviceCode",
+];
+module.exports = {
+    up: (queryInterface, Sequelize) => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
+        for (const modelName of OIDCModelNames) {
+            yield queryInterface.createTable(modelName, Object.assign(Object.assign(Object.assign(Object.assign({ id: { type: STRING, primaryKey: true } }, (OIDCGrantModelNames.includes(modelName) ? { grantId: { type: STRING } } : undefined)), (modelName === "DeviceCode" ? { userCode: { type: STRING } } : undefined)), (modelName === "Session" ? { uid: { type: STRING } } : undefined)), { data: { type: JSON }, expiresAt: { type: DATE }, consumedAt: { type: DATE }, createdAt: { type: DATE }, updatedAt: { type: DATE } }));
+        }
+    }),
+    down: (queryInterface, Sequelize) => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
+        for (const modelName of OIDCModelNames) {
+            yield queryInterface.dropTable(modelName);
+        }
+    })
+};
+//# sourceMappingURL=20191127022419-initialModels.js.map
